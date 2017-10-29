30d06ff7fb547acdff2c028d731f3e04e54ff100
Kento Watanabe <kento621@gmail.com>
2017-10-29 21:40:30 +0900

    Init

---
+++ Inc/93CXX.h
@@ -0,0 +1,38 @@
+﻿
+
+
+#include "stm32f3xx_hal.h"
+
+
+
+
+/*	Define EEPROM TYPE	*
+ * 			93CXX		*/
+#define C86
+//#define C56
+//#define C46
+
+
+#define CMD_READ 	0x02
+#define CMD_WEN 	0x00
+#define CMD_WRITE 	0x01
+
+
+#ifdef C46
+	#define CMD_SHIFT	9
+#elif defined (C56)
+	#define CMD_SHIFT	11
+#elif defined (C86)
+	#define CMD_SHIFT	13
+#endif
+
+
+#define MEM_DelayNUM	2
+#define MEM_DeLay()		for(uint16_t i=0;i<MEM_DelayNUM;i++)asm("NOP")
+#define MEM_Timeout		1000
+
+
+void MEM_writeData(int16_t address,int8_t data);
+int16_t MEM_readData(int16_t address);
+void MEM_WDIS();
+void MEM_WEN();
---
+++ Src/93CXX.c
@@ -0,0 +1,91 @@
+﻿
+
+
+#include "93CXX.h"
+#include "spi.h"
+
+
+void sendCommand(int8_t cmd, int16_t address);
+void checkReady();
+
+
+int16_t MEMrData[1];
+int16_t MEMsData[1];
+
+
+int16_t spi_transfer(int16_t data)
+{
+	MEMsData[0]=data;
+	HAL_SPI_TransmitReceive(&hspi1,
+								(uint8_t*)&MEMsData,
+								(uint8_t*)&MEMrData,
+								1,1000);
+	return MEMrData[0];
+}
+
+void sendCommand(int8_t cmd, int16_t address)
+{
+
+	int16_t sendcmd;
+	sendcmd = (1 << CMD_SHIFT) | (cmd << (CMD_SHIFT-2)) | address;
+	spi_transfer((int8_t)(sendcmd >> 8));
+	MEM_DeLay();
+	spi_transfer((int8_t)(sendcmd & 0xff));
+	MEM_DeLay();
+
+}
+
+void checkReady()
+{
+	uint16_t timecnt=0;
+
+	if(spi_transfer(0)==0) {
+		HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,0);
+		HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,1);
+
+		while(spi_transfer(0)==0) {
+			MEM_DeLay();
+			timecnt++;
+			if(timecnt>MEM_Timeout)break;
+    	}
+		HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,0);
+		HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,1);
+  }
+}
+
+void MEM_writeData(int16_t address,int8_t data)
+{
+  HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,1);
+  checkReady();
+  sendCommand(CMD_WRITE,address);
+  spi_transfer(data);
+  HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,0);
+}
+
+int16_t MEM_readData(int16_t address)
+{
+	int16_t res;
+
+	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,1);
+	checkReady();
+	sendCommand(CMD_READ,address);
+	res = spi_transfer(0xff);
+	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,0);
+	return res;
+}
+
+void MEM_WEN(){
+
+	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,1);
+	sendCommand(CMD_WEN,0x600);
+	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,0);
+
+}
+
+void MEM_WDIS(){
+
+	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,1);
+	sendCommand(CMD_WEN,0x000);
+	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin,0);
+
+}
